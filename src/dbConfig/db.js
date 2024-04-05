dca7f0704a47f375d1c8633c06aa0693e3475c2a
yashG0 <yashgaurkar0@gmail.com>
2024-04-05 18:19:21 +0530

    db fix1

--- src/dbConfig/db.js
+++ src/dbConfig/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 const connectToDatabase = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        const conn = await mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
         console.log("Connected to MongoDB: ",conn.host);
     } catch (error) {
         console.error("Failed to connect to MongoDB:", error.message);
